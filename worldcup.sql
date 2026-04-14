b7cae6a0679e25a05b96dd366ab58a857dde1ebe
jivankhengle <jivankhengle82@gmail.com>
2026-04-14 15:01:13 +0530

    Add World Cup statistics and match results

---
+++ worldcup.sql
@@ -0,0 +1,98 @@
+
+Total number of goals in all games from winning teams:
+68
+
+Total number of goals in all games from both teams combined:
+90
+
+Average number of goals in all games from the winning teams:
+2.1250000000000000
+
+Average number of goals in all games from the winning teams rounded to two decimal places:
+2.13
+
+Average number of goals in all games from both teams:
+2.8125000000000000
+
+Most goals scored in a single game by one team:
+7
+
+Number of games where the winning team scored more than two goals:
+6
+
+Winner of the 2018 tournament team name:
+France
+
+List of teams who played in the 2014 'Eighth-Final' round:
+Algeria
+Argentina
+Belgium
+Brazil
+Chile
+Colombia
+Costa Rica
+France
+Germany
+Greece
+Mexico
+Netherlands
+Nigeria
+Switzerland
+United States
+Uruguay
+
+List of unique winning team names in the whole data set:
+Argentina
+Belgium
+Brazil
+Colombia
+Costa Rica
+Croatia
+England
+France
+Germany
+Netherlands
+Russia
+Sweden
+Uruguay
+
+Year and team name of all the champions:
+2014|Germany
+2018|France
+
+List of teams that start with 'Co':
+Colombia
+Costa Rica
+year,round,winner,opponent,winner_goals,opponent_goals
+2018,Final,France,Croatia,4,2
+2018,Third Place,Belgium,England,2,0
+2018,Semi-Final,Croatia,England,2,1
+2018,Semi-Final,France,Belgium,1,0
+2018,Quarter-Final,Croatia,Russia,3,2
+2018,Quarter-Final,England,Sweden,2,0
+2018,Quarter-Final,Belgium,Brazil,2,1
+2018,Quarter-Final,France,Uruguay,2,0
+2018,Eighth-Final,England,Colombia,2,1
+2018,Eighth-Final,Sweden,Switzerland,1,0
+2018,Eighth-Final,Belgium,Japan,3,2
+2018,Eighth-Final,Brazil,Mexico,2,0
+2018,Eighth-Final,Croatia,Denmark,2,1
+2018,Eighth-Final,Russia,Spain,2,1
+2018,Eighth-Final,Uruguay,Portugal,2,1
+2018,Eighth-Final,France,Argentina,4,3
+2014,Final,Germany,Argentina,1,0
+2014,Third Place,Netherlands,Brazil,3,0
+2014,Semi-Final,Argentina,Netherlands,1,0
+2014,Semi-Final,Germany,Brazil,7,1
+2014,Quarter-Final,Netherlands,Costa Rica,1,0
+2014,Quarter-Final,Argentina,Belgium,1,0
+2014,Quarter-Final,Brazil,Colombia,2,1
+2014,Quarter-Final,Germany,France,1,0
+2014,Eighth-Final,Brazil,Chile,2,1
+2014,Eighth-Final,Colombia,Uruguay,2,0
+2014,Eighth-Final,France,Nigeria,2,0
+2014,Eighth-Final,Germany,Algeria,2,1
+2014,Eighth-Final,Netherlands,Mexico,2,1
+2014,Eighth-Final,Costa Rica,Greece,2,1
+2014,Eighth-Final,Argentina,Switzerland,1,0
+2014,Eighth-Final,Belgium,United States,2,1
